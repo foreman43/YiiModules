86c4f7a0cf49d6a2d61e37c554ba9316d352403b
Артём Лукьянов <a-lukyanov@bk.ru>
2023-09-09 16:43:24 +0400

    city model generated

---
+++ modules/city/models/City.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\modules\city\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $date_create
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['date_create'], 'safe'],
+            [['name'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'date_create' => 'Date Create',
+        ];
+    }
+}
