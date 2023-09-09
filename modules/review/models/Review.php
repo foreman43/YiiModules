30cdec6bfc4b4559377f40b5c1d27113abfedb9c
Артём Лукьянов <a-lukyanov@bk.ru>
2023-09-09 16:11:43 +0400

    Review model & CRUD generated

---
+++ modules/review/models/Review.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\modules\review\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "review".
+ *
+ * @property int $id
+ * @property int $id_city
+ * @property string $title
+ * @property string $text
+ * @property int $rating
+ * @property resource|null $img
+ * @property int $id_author
+ * @property string $date_create
+ */
+class Review extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'review';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id_city', 'rating', 'id_author'], 'integer'],
+            [['title', 'text', 'id_author'], 'required'],
+            [['img'], 'string'],
+            [['date_create'], 'safe'],
+            [['title'], 'string', 'max' => 100],
+            [['text'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'id_city' => 'Id City',
+            'title' => 'Title',
+            'text' => 'Text',
+            'rating' => 'Rating',
+            'img' => 'Img',
+            'id_author' => 'Id Author',
+            'date_create' => 'Date Create',
+        ];
+    }
+}
--- modules/review/views/default/index.php
+++ modules/review/views/default/index.php
@@ -1,12 +1,35 @@
-<div class="review-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
+<?php
+
+use app\modules\review\models\Review;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\widgets\ListView;
+
+/** @var yii\web\View $this */
+/** @var app\modules\review\models\ReviewSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Reviews';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="review-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
     <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
+        <?= Html::a('Create Review', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model->title), ['view', 'id' => $model->id]);
+        },
+    ]) ?>
+
+
 </div>
