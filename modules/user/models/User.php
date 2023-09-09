d86468baf08232b9c25fd95b7c89b5f7a9b89abd
Артём Лукьянов <a-lukyanov@bk.ru>
2023-09-09 13:26:40 +0400

    user model created

---
+++ modules/user/models/User.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace app\modules\user\models;
+
+use Yii;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $patronymic
+ * @property string $email
+ * @property string $phone
+ * @property string $date_create
+ * @property string|null $password
+ */
+class User extends \yii\db\ActiveRecord implements IdentityInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['first_name', 'last_name', 'email', 'phone'], 'required'],
+            [['date_create'], 'safe'],
+            [['first_name', 'last_name', 'patronymic'], 'string', 'max' => 25],
+            [['email'], 'string', 'max' => 30],
+            [['phone'], 'string', 'max' => 15],
+            [['password'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'patronymic' => 'Patronymic',
+            'email' => 'Email',
+            'phone' => 'Phone',
+            'date_create' => 'Date Create',
+            'password' => 'Password',
+        ];
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password): bool
+    {
+        return Yii::$app->security->validatePassword($password, $this->password);
+    }
+
+    public function avaliableUsername($email)
+    {
+        return self::findOne(['email' => $email])->cache(3600) === null;
+    }
+
+    public static function findIdentity($id)
+    {
+        return self::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return self::find()->where(['access_token'=>$token])->one();
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $email
+     * @return static|null
+     */
+    public static function findByEmail(string $email)
+    {
+        return self::findOne(['email'=>$email]);
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey(): string
+    {
+        return $this->auth_key;
+    }
+
+    public function validateAuthKey($authKey): bool
+    {
+        return $this->auth_key === $authKey;
+    }
+}
