4f0ae5e5298d0d913ea3c9ad499bd1dfb779fae4
Артём Лукьянов <a-lukyanov@bk.ru>
2023-09-17 13:03:33 +0400

    Implemented: modal window with contain author info

--- modules/review/views/default/_item.php
+++ modules/review/views/default/_item.php
@@ -5,14 +5,40 @@ use yii\helpers\Url;
 use yii\helpers\StringHelper;
 
 /** @var \app\modules\review\Review $model */
+$authorFullName = "{$model->author->last_name} {$model->author->first_name} {$model->author->patronymic}";
 ?>
 <div>
     <a href="<?php echo Url::to(['/review/default/view', 'id'=>$model->id]) ?>"><h3><?php echo Html::encode($model->title); ?></h3></a>
     <div>
         <?php
         echo StringHelper::truncateWords($model->getEncodedText(),50);
-        echo "<p class=\"text-muted\">{$model->author->last_name} {$model->author->first_name[0]}. </p>";
+        echo "<p>
+            <a class=\"link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover\" data-bs-toggle=\"modal\" data-bs-target=\"#exampleModal\">
+                {$model->author->last_name} {$model->author->first_name[0]}. 
+            </a>
+        </p>";
         ?>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"><?php echo $authorFullName ?></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <?php
+                    echo "<p>Email: {$model->author->email}</p>";
+                    echo "<p>Телефон: {$model->author->phone}</p>";
+                    ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                    <button type="button" class="btn btn-primary">Отзывы этого автора</button>
+                </div>
+            </div>
+        </div>
     </div>
     <hr>
 </div>
