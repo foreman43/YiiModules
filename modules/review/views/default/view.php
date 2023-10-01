2e2c05f577b17c9d57e7ed69753cb2bbfe17b981
Артём Лукьянов <a-lukyanov@bk.ru>
2023-10-01 15:43:41 +0400

    added display of information about author and rating

--- modules/review/views/default/view.php
+++ modules/review/views/default/view.php
@@ -17,10 +17,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?php
-        if(!Yii::$app->user->id == $model->id_author)
+        if(Yii::$app->user->id == $model->id_author)
         {
-            Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']);
-            Html::a('Delete', ['delete', 'id' => $model->id], [
+            echo Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']);
+            echo Html::a('Delete', ['delete', 'id' => $model->id], [
                 'class' => 'btn btn-danger',
                 'data' => [
                     'confirm' => 'Are you sure you want to delete this item?',
@@ -33,7 +33,12 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p class="text-muted">
         <small>
-            Created: <?php echo Yii::$app->formatter->asRelativeTime($model->date_create) ?>
+            Создано: <?php echo Yii::$app->formatter->asRelativeTime($model->date_create) ?><br>
+            Автор: <?php echo Yii::$app->user->isGuest
+                ? "{$model->author->last_name} {$model->author->first_name}"
+                : "<a class=\"link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover\" data-bs-toggle=\"modal\" data-bs-target=\"#exampleModal\">
+                {$model->author->last_name} {$model->author->first_name} </a>"; ?><br>
+            Оценка: <?php echo $model->rating ?>
         </small>
     </p>
 
